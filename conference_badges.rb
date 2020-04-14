df7e4e9d987ef89fb7c7fba512c6504c23458681
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 17:37:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  badge_array = []
   array.each do |name|
-    return "Hello, my name is #{name}."
+    badge_array << "Hello, my name is #{name}."
   end
+  return badge_array
+  
 end
