2ed6757e80408e0e8088f934184c4028412fd01c
Megan Comeau <mlcomeau99@gmail.com>
2020-04-14 18:25:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(array)
 end
 
 def printer (array)
-  batch_badge_creator(array)
-  assign_rooms(array)
+  batch_badge_creator(array).each {|element| puts element}
+  assign_rooms(array).each {|element| puts element}
 end
